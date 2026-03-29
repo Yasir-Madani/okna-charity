b59c75c481f0668f34496f15383eda9afae3fa7c
Yasir Madani <y.madani3@gmail.com>
2026-03-29 16:48:14 +0300

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Tajawal } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const tajawal = Tajawal({
-  subsets: ["arabic", "latin"],
-  weight: ["400", "500", "700"], // حسب الحاجة
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -18,10 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar">
-      <body className={`${tajawal.className} antialiased min-h-full flex flex-col`}>
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
